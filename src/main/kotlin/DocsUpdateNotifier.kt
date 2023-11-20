42c413113582666b4cff798b682863f15a1596ed
Binozo <entwickler@binozoworks.de>
2023-11-20 18:03:35 +0100

    updated year

--- src/main/kotlin/DocsUpdateNotifier.kt
+++ src/main/kotlin/DocsUpdateNotifier.kt
@@ -28,7 +28,7 @@ class DocsUpdateNotifier(propertyFileHandler: PropertyFileHandler) {
         }
         jda = JDABuilder
             .createDefault(discordToken)
-            .setActivity(Activity.playing("WRO 2023")) // optional
+            .setActivity(Activity.playing("WRO 2024")) // optional
             .build().awaitReady()
 
         //read text channel id
